a3a5730babac4b0e72b6e7e6f5b07b1be0cf0369
Sachin Pundhir <pundhir@lobster.bric.dk>
2017-05-25 16:23:15 +0200

    Update

--- share/R/findMergeThreshold.R
+++ share/R/findMergeThreshold.R
@@ -3,7 +3,8 @@ suppressPackageStartupMessages(library("optparse"))
 
 ## parse command line arguments
 option_list <- list(
-	make_option(c("-i", "--inFile"), help="input file containing peak statistics from bed2merge (can be stdin)")
+	make_option(c("-i", "--inFile"), help="input file containing peak statistics from bed2merge (can be stdin)"),
+	make_option(c("-m", "--max"), help="maximum difference point as the threshold (default: 75% quantile)", action="store_true")
 )
 
 parser <- OptionParser(usage = "%prog [options]", option_list=option_list)
@@ -30,16 +31,31 @@ if(identical(opt$inFile, "stdin")==T) {
 
 df$density <- df$V2/df$V3
 z <- df$density
-slope_threshold <- z[which(abs(diff(z))==max(abs(diff(z))))]
-cat(max(df[which(df$density<=slope_threshold),]$V1))
 
 outFile <- sprintf("%s.pdf", opt$inFile)
-pdf(outFile)
-barplot(df$density, names.arg = df$V1, las=2, ylim=c(0,1))
-plot(z)
-abline( v = which(abs(diff(z))==max(abs(diff(z))) ) )
-abline( h = z[which(abs(diff(z))==max(abs(diff(z))) ) ] )
-plot(diff(z))
-invisible(dev.off())
+
+if(is.null(opt$max)) {
+    z <- z[1:which(abs(diff(z))==max(abs(diff(z))))]
+    cat(max(df[which(df$density<=as.vector(summary(z)[5])),]$V1))
+
+    pdf(outFile, width=15)
+    df$color <- "black"
+    df[which(df$density<as.vector(summary(z)[5])),]$color <- "red"
+    barplot(df$density, names.arg = df$V1, las=2, ylim=c(0,1), col=df$color, xlab="p-value (at which to merge peaks)", ylab="# reads within peaks / # mapped reads")
+    invisible(dev.off())
+} else {
+    slope_threshold <- z[which(abs(diff(z))==max(abs(diff(z))))]
+    cat(max(df[which(df$density<slope_threshold),]$V1))
+
+    pdf(outFile, width=15)
+    df$color <- "black"
+    df[which(df$density<slope_threshold),]$color <- "red"
+    barplot(df$density, names.arg = df$V1, las=2, ylim=c(0,1), col=df$color, xlab="p-value (at which to merge peaks)", ylab="# reads within peaks / # mapped reads")
+    #plot(z)
+    #abline( v = which(abs(diff(z))==max(abs(diff(z))) ) )
+    #abline( h = z[which(abs(diff(z))==max(abs(diff(z))) ) ] )
+    #plot(diff(z))
+    invisible(dev.off())
+}
 
 q()
