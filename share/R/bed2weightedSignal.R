6a82990fa20742195cd67da5322f71aa70399f7c
Sachin Pundhir <pundhir@lobster.bric.dk>
2019-03-12 14:58:22 +0100

    Update Tue Mar 12 14:58:07 CET 2019

--- share/R/bed2weightedSignal.R
+++ share/R/bed2weightedSignal.R
@@ -3,10 +3,11 @@ suppressPackageStartupMessages(library("optparse"))
 
 ## parse command line arguments
 option_list <- list(
-	make_option(c("-i", "--inFile"), help="input file containing score values (can be stdin) (format: tab separated <coor><segment><scores>distance>)"),
+	make_option(c("-i", "--inFile"), help="input file containing score values (can be stdin) (format: <gene coor> score (...) distance [class]; scores can be comma separated)"),
     make_option(c("-s", "--scoreCol"), help="column in input file that contains score information. If multiple, separate them by a comma"),
     make_option(c("-d", "--distanceCol"), help="column in input file that contains distance information"),
-    make_option(c("-t", "--segmentCol"), help="column in input file that contains genome segment information compted by ChromHMM")
+    make_option(c("-t", "--segmentCol"), help="column in input file that contains enhancer class information (eg. computed by ChromHMM | active, primed etc)"),
+    make_option(c("-w", "--weight"), default=1000, help="weight parameter, higher it is less steep would be the reduction in signal with increase in distance to TSS (default: %default)")
 )
 
 parser <- OptionParser(usage = "%prog [options]", option_list=option_list)
@@ -33,7 +34,27 @@ if(identical(opt$inFile, "stdin")==T) {
     df <- read.table(opt$inFile)
 }
 
+opt$distanceCol <- as.numeric(opt$distanceCol)
+
+#############################################
+## function to compute total signal
+#############################################
+compute_total_signal <- function(signal) {
+    if(is.na(signal)) {
+        total_signal <- 0
+    } else {
+        total_signal <- sum(as.numeric(unlist(strsplit(as.character(signal), ","))))
+    }
+    return(total_signal)
+}
+
+#############################################
 ## function to compute weighted signal
+## Idea source: https://www.ncbi.nlm.nih.gov/pubmed/27626379
+## e <- exp(1);
+## mu <- 2;
+## plot(unlist(lapply(1:50000, function(dist) { dist <- dist/(10*1000); (2*(e^(-mu*dist)))/(1+((e^(-mu*dist)))); })), xlab="dist_to_tss", ylab="weight", ylim=c(0,1))
+#############################################
 compute_weighted_signal <- function(signal, distance, mu) {
     # signal <- df[3,8]
     # distance <- df[3,9]
@@ -41,7 +62,7 @@ compute_weighted_signal <- function(signal, distance, mu) {
         weighted_signal <- 0
     } else {
         all_signal <- as.numeric(unlist(strsplit(as.character(signal),",")))
-        all_distance <- as.numeric(unlist(strsplit(as.character(distance),",")))/1000
+        all_distance <- as.numeric(unlist(strsplit(as.character(distance),",")))/as.numeric(opt$weight)
 
         e <- exp(1)
         weighted_signal=0
@@ -53,36 +74,9 @@ compute_weighted_signal <- function(signal, distance, mu) {
     return(weighted_signal)
 }
 
-## function to compute total signal
-compute_total_signal <- function(signal) {
-    if(is.na(signal)) {
-        total_signal <- 0
-    } else {
-        total_signal <- sum(as.numeric(unlist(strsplit(as.character(signal), ","))))
-    }
-    return(total_signal)
-}
-
-## function to compute signal count
-compute_count_signal <- function(signal) {
-    if(is.na(signal)) {
-        count_signal <- 0
-    } else {
-        count_signal <- length(unlist(strsplit(as.character(signal), ",")))
-    }
-    return(count_signal)
-}
-
-#opt$scoreCol <- as.numeric(opt$scoreCol)
-opt$distanceCol <- as.numeric(opt$distanceCol)
-
-#df$weighted_signal <- 0
-#df.sub <- df[which(!is.na(df[,opt$scoreCol])),]
-
-## MU is set to 2kb
-#df.sub$weighted_signal <- apply(df.sub, 1, function(x) compute_weighted_signal(x[opt$scoreCol], x[opt$distanceCol], 2))
-#df[which(!is.na(df[,opt$scoreCol])),]$weighted_signal <- df.sub$weighted_signal
-
+#############################################
+## compute total signal
+#############################################
 j=ncol(df)+1
 for(i in unlist(strsplit(opt$scoreCol, ","))) {
     i <- as.numeric(i)
@@ -90,6 +84,9 @@ for(i in unlist(strsplit(opt$scoreCol, ","))) {
     j=j+1
 }
 
+#############################################
+## compute weighted signal
+#############################################
 j=ncol(df)+1
 for(i in unlist(strsplit(opt$scoreCol, ","))) {
     i <- as.numeric(i)
@@ -98,25 +95,45 @@ for(i in unlist(strsplit(opt$scoreCol, ","))) {
     j=j+1
 }
 
-#j=ncol(df)+1
-#for(i in unlist(strsplit(opt$scoreCol, ","))) {
-#    i <- as.numeric(i)
-#    df[,j] <- apply(df, 1, function(x) compute_count_signal(x[i]))
-#    j=j+1
-#}
+#############################################
+## compute enhancer count
+#############################################
+COUNT <- unlist(lapply(df[,opt$distanceCol], function(x) length(unlist(strsplit(as.character(x), ",")))))
+df <- cbind(df, COUNT)
 
+#############################################
+## compute enhancer class information, if required
+#############################################
 if(!is.null(opt$segmentCol)) {
     opt$segmentCol <- as.numeric(opt$segmentCol)
-    segments <- c("enhancer_active", "enhancer_poised", "enhancer_primed", "promoter_active", 
-                "promoter_bivalent", "repressed_heterochromatin", "repressed_polycomb", "transcribed")
+    segments <- unique(unlist(lapply(df[,opt$segmentCol], function(x) unique(unlist(strsplit(as.character(x), ","))))))
+    #segments <- c("enhancer_active", "enhancer_poised", "enhancer_primed", "promoter_active", 
+    #            "promoter_bivalent", "repressed_heterochromatin", "repressed_polycomb", "transcribed")
 
-    df_segments <- t(apply(df, 1, function(x) merge(plyr::count(segments), plyr::count(unlist(strsplit(as.character(x[11]),","))), by="x", all=T)$freq.y[1:length(segments)]))
+    df_segments <- t(apply(df, 1, function(x) merge(plyr::count(segments), plyr::count(unlist(strsplit(as.character(x[opt$segmentCol]),","))), by="x", all=T)$freq.y[1:length(segments)]))
+    colnames(df_segments) <- plyr::count(unlist(strsplit(as.character(df[,opt$segmentCol]),",")))$x
+
     df <- cbind(df, df_segments)
-    write.table(df[,c(1:(opt$segmentCol-1),(opt$distanceCol+1):ncol(df))], "", sep="\t", row.names=F, col.names=F, quote=F)
-} else {
-    write.table(df[,c(1:(as.numeric(unlist(strsplit(opt$scoreCol, ",")))[1]-1),(opt$distanceCol+1):ncol(df))], "", sep="\t", row.names=F, col.names=F, quote=F)
+    #df <- df[,c(1:(opt$segmentCol-1),(opt$distanceCol+1):ncol(df))]
 }
 
-#barplot(df[order(-df$weighted_signal),]$weighted_signal)
+#############################################
+## organize final output results
+#############################################
+#print(which(!seq(1:ncol(df)) %in% c(as.numeric(unlist(strsplit(opt$scoreCol, ","))), opt$distanceCol, opt$segmentCol)))
+#df_output <- df[,c(which(!seq(1:ncol(df)) %in% c(as.numeric(unlist(strsplit(opt$scoreCol, ","))), opt$distanceCol, opt$segmentCol)))]
+if(!is.null(opt$segmentCol)) {
+    #print(c(1:(as.numeric(unlist(strsplit(opt$scoreCol, ",")))[1]-1),(opt$distanceCol+1):ncol(df)))
+    df_output <- df[,c(1:(as.numeric(unlist(strsplit(opt$scoreCol, ",")))[1]-1),(opt$distanceCol+2):ncol(df))]
+} else {
+    df_output <- df[,c(1:(as.numeric(unlist(strsplit(opt$scoreCol, ",")))[1]-1),(opt$distanceCol+1):ncol(df))]
+}
+
+#############################################
+## write final results
+#############################################
+write.table(df_output, "", sep="\t", row.names=F, col.names=T, quote=F)
+
+#barplot(df_output[order(-df$weighted_signal),]$weighted_signal)
 
 q()
