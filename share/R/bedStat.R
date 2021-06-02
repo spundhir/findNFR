93b6507a166799fd198930094312d04cb4a67f0c
Sachin Pundhir <xfd783@briclinux04.sund.root.ku.dk>
2021-06-02 09:55:48 +0200

    Update Wed Jun  2 09:55:33 CEST 2021

--- share/R/bedStat.R
+++ share/R/bedStat.R
@@ -27,17 +27,20 @@ if(identical(opt$bedFile, "stdin")==T) {
     data <- read.table(opt$bedFile)
 }
 
+## check, if file has header
+if(grepl("chr.*start.*end", paste(data[1,], collapse = " ")) == T) { header <- data[1,]; data <- data[-1,]; colnames(data) <- header; }
+
 if(!is.null(opt$list)){
-    cat(sprintf("Median=%0.0f\n", median(data$V1)))
-    cat(sprintf("Mean=%0.0f\n", mean(data$V1)))
-    cat(sprintf("Sum=%0.0f\n", sum(data$V1)))
-    cat(sprintf("Max=%0.0f\n", max(data$V1)))
-    cat(sprintf("Min=%0.0f\n", min(data$V1)))
-    cat(sprintf("Count=%0.0f\n", length(data$V1)))
+    cat(sprintf("Median=%0.0f\n", median(as.numeric(data[,1]))))
+    cat(sprintf("Mean=%0.0f\n", mean(as.numeric(data[,1]))))
+    cat(sprintf("Sum=%0.0f\n", sum(as.numeric(data[,1]))))
+    cat(sprintf("Max=%0.0f\n", max(as.numeric(data[,1]))))
+    cat(sprintf("Min=%0.0f\n", min(as.numeric(data[,1]))))
+    cat(sprintf("Count=%0.0f\n", length(as.numeric(data[,1]))))
 }else{
-    cat(sprintf("Median=%0.0f\n", median(data$V3-data$V2)))
-    cat(sprintf("Mean=%0.0f\n", mean(data$V3-data$V2)))
-    cat(sprintf("Max=%0.0f\n", max(data$V3-data$V2)))
-    cat(sprintf("Min=%0.0f\n", min(data$V3-data$V2)))
-    cat(sprintf("Count=%0.0f\n", length(data$V3-data$V2)))
+    cat(sprintf("Median=%0.0f\n", median(as.numeric(data[,3])-as.numeric(data[,2]))))
+    cat(sprintf("Mean=%0.0f\n", mean(as.numeric(data[,3])-as.numeric(data[,2]))))
+    cat(sprintf("Max=%0.0f\n", max(as.numeric(data[,3])-as.numeric(data[,2]))))
+    cat(sprintf("Min=%0.0f\n", min(as.numeric(data[,3])-as.numeric(data[,2]))))
+    cat(sprintf("Count=%0.0f\n", length(as.numeric(data[,3])-as.numeric(data[,2]))))
 }
