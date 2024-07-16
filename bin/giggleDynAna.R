aa21459c9462a9d85b94f297a93b087767502687
Sachin Pundhir <pundhir.cph@gmail.com>
2024-07-16 14:33:48 +0200

    Update Tue Jul 16 14:33:46 CEST 2024

--- bin/giggleDynAna.R
+++ bin/giggleDynAna.R
@@ -3,19 +3,18 @@ suppressPackageStartupMessages(library("optparse"))
 
 ## parse command line arguments
 option_list <- list(
-	make_option(c("-i", "--inFile"), help="input file containing tf enrichment dynamics (can be stdin)"),
-	make_option(c("-o", "--outPdfFile"), help="output pdf image file"),
-    make_option(c("-c", "--topN"), default="10", help="number of top hits to analyze from each sample (defaut=%default)"),
-    make_option(c("-x", "--pVal"), default="1e-15", help="p-value cutoff (defaut=%default)"),
-    make_option(c("-y", "--minOverlap"), default="50", help="minimum frequency of overlaps in each class (defaut=%default)"),
-    make_option(c("-z", "--minOddsRatio"), default="8", help="minimum odds ratio (log) of at least one class (defaut=%default)"),
-    make_option(c("-c", "--color"), default="RdBu", help="color palette (default=%default)"),
-    make_option(c("-b", "--colorBias"), default="1", help="bias in color (default=%default)"),
-    make_option(c("-l", "--mustInclude"), help="name of element that must be included in the final output (if multiple, separate them by a comma)"),
-    make_option(c("-R", "--clusterRows"), default=F, help="cluster rows in the heatmap (default=%default)"),
-    make_option(c("-C", "--clusterCols"), default=T, help="cluster columns in the heatmap (default=%default)"),
-    make_option(c("-W", "--plotWidth"), default=5, help="width of the heatmap (default=%default)"),
-    make_option(c("-H", "--plotHeight"), default=5, help="height of the heatmap (default=%default)")
+  make_option(c("-i", "--inFile"), help="input file containing tf enrichment dynamics (can be stdin)"),
+  make_option(c("-o", "--outPdfFile"), help="output pdf image file"),
+  make_option(c("-c", "--topN"), default=10, help="number of top hits to analyze from each sample (defaut=%default)"),
+  make_option(c("-x", "--pVal"), default=1e-15, help="p-value cutoff (defaut=%default)"),
+  make_option(c("-y", "--minOverlap"), default=50, help="minimum frequency of overlaps in each class (defaut=%default)"),
+  make_option(c("-z", "--minOddsRatio"), default=8, help="minimum odds ratio (log) of at least one class (defaut=%default)"),
+  make_option(c("-b", "--colorBias"), default="1", help="bias in color (default=%default)"),
+  make_option(c("-l", "--mustInclude"), help="name of element that must be included in the final output (if multiple, separate them by a comma)"),
+  make_option(c("-R", "--clusterRows"), default=F, help="cluster rows in the heatmap (default=%default)"),
+  make_option(c("-C", "--clusterCols"), default=T, help="cluster columns in the heatmap (default=%default)"),
+  make_option(c("-W", "--plotWidth"), default=5, help="width of the heatmap (default=%default)"),
+  make_option(c("-H", "--plotHeight"), default=5, help="height of the heatmap (default=%default)")
 )
 
 parser <- OptionParser(usage = "%prog [options]", option_list=option_list)
@@ -35,9 +34,12 @@ if(is.null(opt$inFile) | is.null(opt$outPdfFile)) {
 suppressPackageStartupMessages(library(reshape))
 suppressPackageStartupMessages(library(reshape2))
 suppressPackageStartupMessages(library(RColorBrewer))
-suppressPackageStartupMessages(library(gplots))
+suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(session))
 suppressPackageStartupMessages(library(pheatmap))
+suppressPackageStartupMessages(library(Biobase))
+suppressPackageStartupMessages(library(matrixStats))
+suppressPackageStartupMessages(library(dplyr))
 
 ## header of DENOVO- or KNOWN-MOTIF_ENRICHMENT_DYNAMICS.TXT
 # 1.  SAMPLE_ID 
@@ -70,16 +72,16 @@ names(df) <- c("overlaps", "odds_ratio", "combo_score", "fishers_two_tail")
 
 ## identify significant overlaps
 sig_rows <- which(rowMin(as.matrix(df[["fishers_two_tail"]])) < opt$pVal &
-                    rowMin(as.matrix(df[["overlaps"]])) < opt$minOverlap &
+                    rowMin(as.matrix(df[["overlaps"]])) > opt$minOverlap &
                     log(rowMax(round(as.matrix(df[["odds_ratio"]])))) >= opt$minOddsRatio &
                     log(rowSds(as.matrix(df[["odds_ratio"]]))) > 4)
 
-if(!is.null(opt$mustInclude) {
+if(!is.null(opt$mustInclude)) {
     sig_rows = c(sig_rows, which(row.names(df$overlaps) %in% unlist(strsplit(opt$mustInclude, ","))))
 }
 
 ## filter top N overlaps ordered based on combo score
-sig_rows[sig_rows %in% which(row.names(df$combo_score) %in% unique(unlist(lapply(colnames(df$combo_score), function(x) head(row.names(df$combo_score[order(-df$combo_score[,x]),]), opt$topN)))))]
+sig_rows <- sig_rows[sig_rows %in% which(row.names(df$combo_score) %in% unique(unlist(lapply(colnames(df$combo_score), function(x) head(row.names(df$combo_score[order(-df$combo_score[,x]),]), opt$topN)))))]
 
 if(length(sig_rows)>2) {
     df_sig <- cbind(df[["overlaps"]][sig_rows,] %>% mutate(name = row.names(df[["overlaps"]][sig_rows,])) %>% reshape2::melt(),
