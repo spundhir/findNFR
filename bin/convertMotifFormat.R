4e3e399285df3d3a188e6777bd538a82206a633a
Sachin Pundhir <pundhir.cph@gmail.com>
2025-06-06 13:32:46 +0200

    Update Fri Jun  6 13:27:23 CEST 2025

---
+++ bin/convertMotifFormat.R
@@ -0,0 +1,74 @@
+#!/localhome/bric/xfd783/miniconda3/bin/Rscript
+suppressPackageStartupMessages(library("optparse"))
+
+## parse command line arguments
+option_list <- list(
+	make_option(c("-i", "--inFile"), help="input file containing motifs (can be stdin)"),
+	make_option(c("-o", "--outFile"), help="output file containing motifs"),
+    make_option(c("-f", "--inFormat"), default="matrix", help="format of motifs in input file (homer, jaspar, meme, transfac, uniprobe, matrix; default=%default)"),
+    make_option(c("-F", "--outFormat"), default="homer", help="format of motifs in output file (homer, jaspar, meme, transfac, uniprobe, matrix; default=%default)")
+)
+
+parser <- OptionParser(usage = "%prog [options]", option_list=option_list)
+opt <- parse_args(parser)
+
+## check, if all required arguments are given
+if(is.null(opt$inFile) | is.null(opt$outFile)) {
+	cat("\nProgram: convertMotifFormat.R (R script to convert format of motifs)\n")
+	cat("Author: BRIC, University of Copenhagen, Denmark\n")
+	cat("Version: 1.0\n")
+	cat("Contact: pundhir@binf.ku.dk\n");
+	print_help(parser)
+	q()
+}
+
+## load libraries
+suppressPackageStartupMessages(library(universalmotif))
+
+## read input motif file
+if(identical(opt$inFile, "stdin")==T) { 
+    if(opt$inFormat=="homer") {
+        df <- suppressWarnings(read_homer(file("stdin")))
+    } else if(opt$inFormat=="jaspar") {
+        df <- suppressWarnings(read_jaspar(file("stdin")))
+    } else if(opt$inFormat=="meme") {
+        df <- suppressWarnings(read_meme(file("stdin"), readsites=T, readsites.meta=T))
+    } else if(opt$inFormat=="transfac") {
+        df <- suppressWarnings(read_transfac(file("stdin")))
+    } else if(opt$inFormat=="uniprobe") {
+        df <- suppressWarnings(read_uniprobe(file("stdin")))
+    } else {
+        df <- suppressWarnings(read_matrix(file("stdin"), positions = "columns", headers = T, rownames = T))
+    }
+} else {
+    if(opt$inFormat=="homer") {
+        df <- suppressWarnings(read_homer(opt$inFile))
+    } else if(opt$inFormat=="jaspar") {
+        df <- suppressWarnings(read_jaspar(opt$inFile))
+    } else if(opt$inFormat=="meme") {
+        df <- suppressWarnings(read_meme(opt$inFile, readsites=T, readsites.meta=T))
+    } else if(opt$inFormat=="transfac") {
+        df <- suppressWarnings(read_transfac(opt$inFile))
+    } else if(opt$inFormat=="uniprobe") {
+        df <- suppressWarnings(read_uniprobe(opt$inFile))
+    } else {
+        df <- suppressWarnings(read_matrix(opt$inFile, positions = "columns", headers = T, rownames = T))
+    }
+}
+
+## convert motifs into desired format and write to ouput file
+if(opt$outFormat=="homer") {
+    write_homer(df, opt$outFile, overwrite = T)
+} else if(opt$outFormat=="jaspar") {
+    write_jaspar(df, opt$outFile, overwrite = T)
+} else if(opt$outFormat=="meme") {
+    write_meme(df, opt$outFile, version = 5, overwrite = T)
+} else if(opt$outFormat=="transfac") {
+    write_transfac(df, opt$outFile, overwrite = T)
+} else if(opt$outFormat=="uniprobe") {
+    write_uniprobe(df, opt$outFile, overwrite = T)
+} else {
+    write_matrix(df, opt$outFile, positions = "columns", headers = T, rownames = T)
+}
+
+q()
