249fec5f61df66570aee14fe15edc5c1dd49cb9b
Sachin Pundhir <pundhir.cph@gmail.com>
2025-06-10 10:39:29 +0200

    Update Tue Jun 10 10:34:10 CEST 2025

--- bin/convertMotifFormat.R
+++ bin/convertMotifFormat.R
@@ -5,8 +5,8 @@ suppressPackageStartupMessages(library("optparse"))
 option_list <- list(
 	make_option(c("-i", "--inFile"), help="input file containing motifs (can be stdin)"),
 	make_option(c("-o", "--outFile"), help="output file containing motifs"),
-    make_option(c("-f", "--inFormat"), default="matrix", help="format of motifs in input file (homer, jaspar, meme, transfac, uniprobe, matrix; default=%default)"),
-    make_option(c("-F", "--outFormat"), default="homer", help="format of motifs in output file (homer, jaspar, meme, transfac, uniprobe, matrix; default=%default)")
+    make_option(c("-f", "--inFormat"), default="matrix", help="format of motifs in input file (homer, jaspar, meme, transfac, uniprobe, cisbp, matrix; default=%default)"),
+    make_option(c("-F", "--outFormat"), default="homer", help="format of motifs in output file (homer, jaspar, meme, transfac, uniprobe, cisbp, matrix; default=%default)")
 )
 
 parser <- OptionParser(usage = "%prog [options]", option_list=option_list)
@@ -29,34 +29,25 @@ suppressPackageStartupMessages(library(universalmotif))
 ## read input motif file
 if(identical(opt$inFile, "stdin")==T) {
     tmp_file <- tempfile(fileext = ".txt")
+    #tmp_file <- "/localhome/bric/xfd783/software/homer/data/knownTFs/vertebrates/2025_version/t.txt"
     writeLines(readLines(file("stdin")), tmp_file)
-    if(opt$inFormat=="homer") {
-        df <- suppressWarnings(read_homer(tmp_file))
-    } else if(opt$inFormat=="jaspar") {
-        df <- suppressWarnings(read_jaspar(tmp_file))
-    } else if(opt$inFormat=="meme") {
-        df <- suppressWarnings(read_meme(tmp_file, readsites=T, readsites.meta=T))
-    } else if(opt$inFormat=="transfac") {
-        df <- suppressWarnings(read_transfac(tmp_file))
-    } else if(opt$inFormat=="uniprobe") {
-        df <- suppressWarnings(read_uniprobe(tmp_file))
-    } else {
-        df <- suppressWarnings(read_matrix(tmp_file, positions = "columns", headers = T, rownames = T))
-    }
+    opt$inFile <- tmp_file
+}
+
+if(opt$inFormat=="homer") {
+    df <- suppressWarnings(read_homer(opt$inFile))
+} else if(opt$inFormat=="jaspar") {
+    df <- suppressWarnings(read_jaspar(opt$inFile))
+} else if(opt$inFormat=="meme") {
+    df <- suppressWarnings(read_meme(opt$inFile, readsites=T, readsites.meta=T))
+} else if(opt$inFormat=="transfac") {
+    df <- suppressWarnings(read_transfac(opt$inFile))
+} else if(opt$inFormat=="uniprobe") {
+    df <- suppressWarnings(read_uniprobe(opt$inFile))
+} else if(opt$inFormat=="cisbp") {
+    df <- suppressWarnings(read_cisbp(opt$inFile))
 } else {
-    if(opt$inFormat=="homer") {
-        df <- suppressWarnings(read_homer(opt$inFile))
-    } else if(opt$inFormat=="jaspar") {
-        df <- suppressWarnings(read_jaspar(opt$inFile))
-    } else if(opt$inFormat=="meme") {
-        df <- suppressWarnings(read_meme(opt$inFile, readsites=T, readsites.meta=T))
-    } else if(opt$inFormat=="transfac") {
-        df <- suppressWarnings(read_transfac(opt$inFile))
-    } else if(opt$inFormat=="uniprobe") {
-        df <- suppressWarnings(read_uniprobe(opt$inFile))
-    } else {
-        df <- suppressWarnings(read_matrix(opt$inFile, positions = "columns", headers = T, rownames = T))
-    }
+    df <- suppressWarnings(read_matrix(opt$inFile, positions = "columns", headers = T, rownames = T))
 }
 
 ## convert motifs into desired format and write to ouput file
@@ -70,6 +61,8 @@ if(opt$outFormat=="homer") {
     write_transfac(df, opt$outFile, overwrite = T)
 } else if(opt$outFormat=="uniprobe") {
     write_uniprobe(df, opt$outFile, overwrite = T)
+} else if(opt$outFormat=="cisbp") {
+    write_cisbp(df, opt$outFile, overwrite = T)
 } else {
     write_matrix(df, opt$outFile, positions = "columns", headers = T, rownames = T)
 }
