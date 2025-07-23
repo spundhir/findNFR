af66c2804915213dfd98e54385ae7406a284f6f6
Sachin Pundhir <pundhir.cph@gmail.com>
2025-07-23 18:52:31 +0200

    Update Wed Jul 23 18:47:14 CEST 2025

--- bin/loop2domain.R
+++ bin/loop2domain.R
@@ -31,6 +31,8 @@ suppressPackageStartupMessages(library("doBy"))
 suppressPackageStartupMessages(library("data.table"))
 suppressPackageStartupMessages(library("parallel"))
 suppressPackageStartupMessages(library("AnnotationDbi"))
+suppressPackageStartupMessages(library("apcluster"))
+suppressPackageStartupMessages(library("lsa"))
 
 ## read input file
 if(identical(opt$inDistFile, "stdin")==T) {
@@ -102,8 +104,61 @@ mat <- matrix(NA, length(all_genes), length(all_genes), dimnames = list(all_gene
 
 # Fill
 mat[cbind(dt$qGene, dt$sGene)] <- dt$iScore
+mat <- mat[GENES_SORTED,GENES_SORTED]
 
 ## write matrix to stdout
-write.table(mat[GENES_SORTED,GENES_SORTED], file="", col.names=T, row.names=T, quote=F, sep="\t")
+#write.table(mat, file="", col.names=T, row.names=T, quote=F, sep="\t")
+
+## determine domains
+gene_chr <- (df %>% separate_longer_delim(targetGene, delim = ",") %>% dplyr::select(chr, targetGene) %>% unique)
+
+t <- lapply(unique(gene_chr$chr), function(CHR) {
+  mat.sub <- mat[which(colnames(mat) %in% gene_chr[which(gene_chr==CHR),]$targetGene), 
+                 which(colnames(mat) %in% gene_chr[which(gene_chr==CHR),]$targetGene)] %>%
+    replace(is.na(.), 1) %>% log
+  
+  ## APPROACH-1
+  # t <- lsa::cosine(t(mat.sub))
+  # t <- lsa::cosine(t(matrix2norm(t(mat.sub), method = "softmax")))
+  # pheatmap(t, cluster_rows = F, cluster_cols = F, border_color = NA, scale="none")
+  # CI <- cluster_matrix(t, y = 114, usePheatmap = T)
+  # CI$cluster[which(names(CI$cluster)=="Meis1")]
+  # which(CI$cluster==CI$cluster[which(names(CI$cluster)=="Meis1")])
+  # barplot(CI$withinss, names=unique(sort(CI$cluster)), las=2)
+  # barplot(CI$centers[1,])
+  # ggarrange(matrix2Heatmap(mat.sub, y = CI$cluster, clusterRows = F, clusterCols = F))
+  
+  ## APPROACH-2
+  # mat.sub <- mat.sub[row.names(mat.sub[rowSums(mat.sub != 0) > 1, ]),row.names(mat.sub[rowSums(mat.sub != 0) > 1, ])]
+  # ap_result <- apcluster(s = as.matrix(t(mat.sub)), seed=10, maxits=10000)
+  ap_result <- apcluster(cosine, t(mat.sub), seed=10, maxits=10000, details=TRUE)
+  geneCluster <- do.call(rbind, lapply(seq(1:length(ap_result@clusters)), function(i) cbind(names(ap_result@clusters[[i]]), i))) %>% as.data.frame %>% 'colnames<-'(c("gene", "cluster"))
+  geneBlackListed <- lapply(seq(1:length(ap_result@clusters)), function(i) unlist(lapply(names(ap_result@clusters[[i]]), function(x) { if(length(which(mat.sub[x, names(ap_result@clusters[[i]])]>0))==1) { x; } }))) %>% unlist
+  geneCluster[which(geneCluster$gene %in% geneBlackListed),]$cluster <- 0
+  geneCluster <- geneCluster[order(geneCluster$cluster),]
+  geneCluster$cluster <- sprintf("%s_%s", geneCluster$cluster, CHR)
+  geneCluster
+  ## sanity check
+  # GENE="Hoxa9"
+  # geneCluster[which(geneCluster$gene==GENE),]$cluster
+  # geneCluster[which(geneCluster$cluster==geneCluster[which(geneCluster$gene==GENE),]$cluster),]$gene
+  # mat.sub[GENE, geneCluster[which(geneCluster$cluster==geneCluster[which(geneCluster$gene==GENE),]$cluster),]$gene]
+  # mat.sub[GENE,][which(mat.sub[GENE,]>0)]
+  # ggarrange(matrix2Heatmap(mat.sub, y=geneCluster$cluster, clusterRows = F, clusterCols = F))
+  # pheatmap(as.matrix(mat.sub[geneBlackListed,]), cluster_rows = F, cluster_cols = F, border_color = NA)
+  
+  ## APPROACH-3
+  # umap_result <- uwot::umap(t, n_neighbors = 15, min_dist = 0.1, metric = "euclidean")
+  # # Put in data frame for plotting
+  # umap_df <- data.frame(UMAP1 = umap_result[,1],
+  #                       UMAP2 = umap_result[,2],
+  #                       Gene = rownames(mat.sub))
+  # ggscatter(umap_df, x="UMAP1", y="UMAP2", label = "Gene", label.select = "Meis1")
+})
+geneCluster <- do.call(rbind, t)
+# (geneCluster %>% dplyr::filter(cluster==(geneCluster %>% dplyr::filter(gene=="Myc") %>% dplyr::select(cluster))$cluster))$gene
+
+## write gene domains to stdout
+write.table(geneCluster, file="", col.names=T, row.names=F, quote=F, sep="\t")
 
 q()
