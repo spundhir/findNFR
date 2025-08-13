bcb6078e118a613c082eed4a0cb2ade4e53f0374
xfd783 <pundhir.cph@gmail.com>
2025-08-13 12:36:57 +0200

    Update Wed Aug 13 12:36:54 CEST 2025

---
+++ bin/giggleIndex.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import argparse
+from datetime import datetime
+from giggle import Giggle
+
+def main():
+    parser = argparse.ArgumentParser(description="Create a Giggle index from BED files.")
+    parser.add_argument('-i', '--inBed', required=True, help='Path to the input BED files directory')
+    parser.add_argument('-o', '--outIdx', required=True, help='Path to the index output directory')
+    args = parser.parse_args()
+
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    print(f"[{current_time}] Creating Giggle index at {args.outIdx} from BED files in {args.inBed}")
+    # Giggle.create('/home/xfd783/data/09_ALL_PUBLIC/database/unibind/v2022/mm10/test1', '/home/xfd783/data/09_ALL_PUBLIC/database/unibind/v2022/mm10/test/*.bed.gz')
+    Giggle.create(args.outIdx, f"{args.inBed}/*.bed.gz")
+
+if __name__ == "__main__":
+    main()
+
