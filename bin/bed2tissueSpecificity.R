4ac6904519f914ba4523a890752b9398a9d7438f
xfd783 <pundhir.cph@gmail.com>
2025-09-23 11:10:50 +0200

    Update Tue Sep 23 11:10:47 CEST 2025

--- bin/bed2tissueSpecificity.R
+++ bin/bed2tissueSpecificity.R
@@ -5,7 +5,8 @@ suppressPackageStartupMessages(library("optparse"))
 ## parse command line arguments
 ##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@##
 option_list <- list(
-	make_option(c("-i", "--inFile"), help="bed file (can be stdin) [format: chr start end name score (comma sep.) strand tissue (comma sep.)]")
+	make_option(c("-i", "--inFile"), help="bed file (can be stdin) [format: chr start end name score (comma sep.) strand tissue (comma sep.)]"),
+	make_option(c("-M", "--useMeuleman"), action="store_true", help="compute using data from Meuleman et al. 2020")
 )
 
 parser <- OptionParser(usage = "%prog [options]", option_list=option_list)
@@ -72,23 +73,45 @@ vector2tau <- function(x)
 ## read input file
 ## @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@##
 if (identical(opt$inFile, "stdin") == T) {
-  df <- read.table(file("stdin"), header = F)[,c(1:7)]
+  df <- read.table(file("stdin"), header = F)
 } else {
-  df <- read.table(opt$inFile, header = F)[,c(1:7)]
+  df <- read.table(opt$inFile, header = F)
 }
-colnames(df) <- c("chr", "start", "end", "name", "score", "strand", "tissue")
+## check if file has header (all elements in the first row are non-numeric)
+if(length(grep("FALSE", unlist(lapply(df[1,], function(x) is.na(suppressWarnings(as.numeric(x)))))))==0) {
+    colnames(df) <- unlist(df[1,]); 
+    df <- df[-1,];
+} else {
+    df <- df[,c(1:7)]
+    colnames(df) <- c("chr", "start", "end", "name", "score", "strand", "tissue")
+}
 #df <- read.table(opt$inFile)[,c(1:7)] %>% 'colnames<-'(c("chr", "start", "end", "name", "score", "strand", "tissue"))
 
 ##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@##
 ## start analysis based on input bed file
 ##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@##
-dfE <- cbind((df %>% separate_longer_delim(score, delim = ",") %>% dplyr::select(!tissue)), (df %>% separate_longer_delim(tissue, delim = ",") %>% dplyr::select(tissue)))
-mat <- matrix(0, length(unique(dfE$name)), length(unique(dfE$tissue)), dimnames = list(unique(dfE$name), unique(dfE$tissue)))
-mat[cbind(dfE$name, dfE$tissue)] <- as.numeric(dfE$score)
-mat <- as.data.frame(mat)
-mat$tau <- apply(log2(mat+1), 1, vector2tau)
-df <- merge(df, mat, by.x="name", by.y="row.names")
-df <- df[,c(2:4,1,5:ncol(df))]
+if(opt$useMeuleman==F) {
+    dfE <- cbind((df %>% separate_longer_delim(score, delim = ",") %>% dplyr::select(!tissue)), (df %>% separate_longer_delim(tissue, delim = ",") %>% dplyr::select(tissue)))
+    mat <- matrix(0, length(unique(dfE$name)), length(unique(dfE$tissue)), dimnames = list(unique(dfE$name), unique(dfE$tissue)))
+    mat[cbind(dfE$name, dfE$tissue)] <- as.numeric(dfE$score)
+    mat <- as.data.frame(mat)
+    mat$tau <- apply(log2(mat+1), 1, vector2tau)
+    df <- merge(df, mat, by.x="name", by.y="row.names")
+    df <- df[,c(2:4,1,5:ncol(df))]
+} else {
+    counts <- fread("~/data/09_ALL_PUBLIC/database/02_accessibilityCatalog/dhsCatalog/hg38/dat_FDR01_hg38.txt.gz", header=F)
+    metaData <- fread("~/data/09_ALL_PUBLIC/database/02_accessibilityCatalog/dhsCatalog/hg38/DHS_Index_and_Vocabulary_metadata.tsv", header=T)
+    
+    mat <- as.data.frame(lapply(unique(metaData[which(metaData$Organ!=""),]$Organ), function(x) {
+                                                                          i <- which(metaData$Organ==x)
+                                                                          rowMeans(counts[,..i])
+                                                                        }
+                                )
+                         )
+    df$tau <- apply(mat, 1, vector2tau)
+    df$gini <- apply(mat, 1, function(x) DescTools::Gini(x, na.rm = T))
+    # ggscatter_with_contour(df, var_x="numsamples", var_y="tau")
+}
 
 ##@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@##
 ## output file containing tissue specificity information
