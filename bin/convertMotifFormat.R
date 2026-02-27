34032256f9c1049553b85037b10c8763fb3bd35e
xfd783 <pundhir.cph@gmail.com>
2026-02-27 13:55:21 +0100

    Update Fri Feb 27 13:55:17 CET 2026

--- bin/convertMotifFormat.R
+++ bin/convertMotifFormat.R
@@ -4,9 +4,9 @@ suppressPackageStartupMessages(library("optparse"))
 ## parse command line arguments
 option_list <- list(
 	make_option(c("-i", "--inFile"), help="input file containing motifs (can be stdin)"),
-	make_option(c("-o", "--outFile"), help="output file containing motifs (optional, default=stdout)"),
     make_option(c("-f", "--inFormat"), default="matrix", help="format of motifs in input file (homer, jaspar, meme, transfac, uniprobe, cisbp, matrix; default=%default)"),
-    make_option(c("-F", "--outFormat"), default="homer", help="format of motifs in output file (homer, jaspar, meme, transfac, uniprobe, cisbp, matrix; default=%default)")
+    make_option(c("-F", "--outFormat"), default="homer", help="format of motifs in output file (homer, jaspar, meme, transfac, uniprobe, cisbp, matrix; default=%default)"),
+	make_option(c("-R", "--reduceScore"), action="store_true", help="reduce score by 4")
 )
 
 parser <- OptionParser(usage = "%prog [options]", option_list=option_list)
@@ -51,30 +51,27 @@ if(opt$inFormat=="homer") {
 }
 
 ## convert motifs into desired format and write to ouput file
-if(is.null(opt$outFile)) {
-    tmpOutFile <- tempfile(fileext = ".txt")
-    opt$outFile <- tmpOutFile
-} else {
-    tmpOutFile <- NULL
-}
+tmpOutFile <- tempfile(fileext = ".txt")
 
 if(opt$outFormat=="homer") {
-    write_homer(df, opt$outFile, overwrite = T)
+    write_homer(df, tmpOutFile, overwrite = T)
 } else if(opt$outFormat=="jaspar") {
-    write_jaspar(df, opt$outFile, overwrite = T)
+    write_jaspar(df, tmpOutFile, overwrite = T)
 } else if(opt$outFormat=="meme") {
-    write_meme(df, opt$outFile, version = 5, overwrite = T)
+    write_meme(df, tmpOutFile, version = 5, overwrite = T)
 } else if(opt$outFormat=="transfac") {
-    write_transfac(df, opt$outFile, overwrite = T)
+    write_transfac(df, tmpOutFile, overwrite = T)
 } else if(opt$outFormat=="uniprobe") {
-    write_uniprobe(df, opt$outFile, overwrite = T)
+    write_uniprobe(df, tmpOutFile, overwrite = T)
 } else if(opt$outFormat=="cisbp") {
-    write_cisbp(df, opt$outFile, overwrite = T)
+    write_cisbp(df, tmpOutFile, overwrite = T)
 } else {
-    write_matrix(df, opt$outFile, positions = "columns", headers = T, rownames = T)
+    write_matrix(df, tmpOutFile, positions = "columns", headers = T, rownames = T)
 }
 
-if(!is.null(tmpOutFile)) {
+if(!is.null(opt$reduceScore)) {
+    cat(readLines(pipe(sprintf("cat %s | awk -v OFS='\t' '{ if($0 ~ /^>/) { print $1,$2,$3-4; } else { print $0; }}'", tmpOutFile))), sep="\n")
+} else {
     cat(readLines(tmpOutFile), sep="\n")
 }
 q()
